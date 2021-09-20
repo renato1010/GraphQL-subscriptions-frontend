bbf7c026707cedc4a388015f31afcd9dc62b9e1d
renato.perez <renatoperezc@gmail.com>
2021-09-20 07:10:19 -0600

    Use locally run server url

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,12 @@ import './index.css';
 import { App } from './App';
 
 const wsLink = new WebSocketLink({
-  uri: 'wss://renato-pickfu.herokuapp.com/',
+  uri: 'ws://localhost:4000/',
   options: {
     reconnect: true,
   },
 });
-const httpLink = new HttpLink({ uri: 'https://renato-pickfu.herokuapp.com/' });
+const httpLink = new HttpLink({ uri: 'http://localhost:4000/' });
 const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
